48536c6829cc47134525f9b6176e535084803325
Amol Jagtap <amoljagtap17@gmail.com>
2022-08-12 20:54:07 +0530

    2022_express_mongo_ts: initial biolerplate code

---
+++ src/app.ts
@@ -0,0 +1,57 @@
+import express, { Application } from 'express';
+import mongoose from 'mongoose';
+import compression from 'compression';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+
+import { IController } from 'types';
+import { errorMiddleware } from 'middlewares';
+
+export class App {
+  public express: Application;
+  public port: number;
+
+  constructor(controllers: IController[], port: number) {
+    this.express = express();
+    this.port = port;
+
+    this.initialiseDatabaseConnection();
+    this.initialiseMiddleware();
+    this.initialiseControllers(controllers);
+    this.initialiseErrorHandling();
+  }
+
+  private initialiseMiddleware(): void {
+    this.express.use(helmet());
+    this.express.use(cors());
+    this.express.use(morgan('dev'));
+    this.express.use(express.json());
+    this.express.use(express.urlencoded({ extended: false }));
+    this.express.use(compression());
+  }
+
+  private initialiseControllers(controllers: IController[]): void {
+    controllers.forEach((controller: IController) => {
+      this.express.use('/api', controller.router);
+    });
+  }
+
+  private initialiseErrorHandling(): void {
+    this.express.use(errorMiddleware);
+  }
+
+  private initialiseDatabaseConnection(): void {
+    const MONGO_USER = process.env.MONGO_USER;
+    const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+    const MONGO_PATH = process.env.MONGO_PATH;
+
+    mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`);
+  }
+
+  public listen(): void {
+    this.express.listen(this.port, () => {
+      console.log(`App listening on the port ${this.port}`);
+    });
+  }
+}
---
+++ src/middlewares/error.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
+import { HttpException } from 'exceptions';
+
+export const errorMiddleware = (
+  error: HttpException,
+  req: Request,
+  res: Response,
+  _next: NextFunction
+): void => {
+  const status = error.status || 500;
+  const message = error.message || 'Something went wrong';
+
+  res.status(status).send({
+    status,
+    message,
+  });
+};
